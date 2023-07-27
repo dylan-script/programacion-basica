fb3c3966edc507a85e5b7273a7f760e8582347b6
Dilan ThinkPad Windows <dilanjimenez140@gmail.com>
2023-07-27 11:55:58 -0500

    etiqueta script en mokepon

---
+++ mokepon/JS/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador(){
+    alert('Seleccionaste tu mascota')
+}
+
+let botonMacotaJuagdor = document.getElementById('boton-mascota')
+botonMacotaJuagdor.addEventListener('click', seleccionarMascotaJugador)
