7ef1d1e396f928320ae99e3a5c2ffe5b61332724
Dilan ThinkPad Windows <dilanjimenez140@gmail.com>
2023-07-27 12:53:44 -0500

    addEventListener

--- mokepon/JS/mokepon.js
+++ mokepon/JS/mokepon.js
@@ -1,10 +1,24 @@
-function iniciarJuego(){
-    let botonMacotaJuagdor = document.getElementById('boton-mascota')
+const $ = selector => document.getElementById(selector)
+
+function iniciarJuego() {
+    let botonMacotaJuagdor = $('boton-mascota')
     botonMacotaJuagdor.addEventListener('click', seleccionarMascotaJugador)
 }
 
-function seleccionarMascotaJugador(){
-    alert('Seleccionaste tu mascota')
+function seleccionarMascotaJugador() {
+    let inputHipodoge = $('hipodoge')
+    let inputCapipepo = $('capipepo')
+    let inputRatigueya = $('ratigueya')
+    if (inputHipodoge.checked) {
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya')
+    } else {
+        alert('Por favor ejige una mascota')
+    }
+
 }
 
 window.addEventListener('load', iniciarJuego)
